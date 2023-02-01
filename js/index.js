12576381b5d59f6073ec98f50674c77f3224a55b
Philip Tabaro <94716119+philiptabaro@users.noreply.github.com>
2023-02-01 14:42:55 -0300

    Added first files

---
+++ js/index.js
@@ -0,0 +1,19 @@
+var randomNumber1 = Math.floor(Math.random() * 7);
+var randomNumber2 = Math.floor(Math.random() * 7);
+
+
+var dicePics = ["/images/dice1.png", "/images/dice2.png", 
+                "/images/dice3.png", "/images/dice4.png", 
+                "/images/dice5.png", "/images/dice6.png"]
+
+document.querySelector(".left-img").src = dicePics[randomNumber1];
+document.querySelector(".right-img").src = dicePics[randomNumber2];
+
+if (randomNumber1 > randomNumber2){
+    document.querySelector("h1").innerHTML = "🚩 Player 1 Wins!"
+
+} else if (randomNumber1 < randomNumber2){
+    document.querySelector("h1").innerHTML = "Player 2 Wins! 🚩"
+} else {
+    document.querySelector("h1").innerHTML = "Draw!"
+}
